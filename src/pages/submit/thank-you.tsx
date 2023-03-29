6ed0e09a185d249bf44434b90585b98a388b4acf
abhishek <abbhishekk27@gmail.com>
2023-03-29 17:31:05 +0530

    fix

--- src/pages/submit/thank-you.tsx
+++ src/pages/submit/thank-you.tsx
@@ -3,7 +3,7 @@ import { ThankYouIcon } from '@/svgs/submit';
 
 export default function Thanks() {
   return (
-    <div className="mx-auto mt-36 min-h-screen max-w-[800px] bg-black md:mt-20">
+    <div className="mx-auto mt-36 min-h-screen max-w-[600px] bg-black md:mt-20">
       <ThankYouIcon />
     </div>
   );
